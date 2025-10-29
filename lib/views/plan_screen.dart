e6992b1057d6cb239926f9d7f7cb6f24729e2241
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-10-29 14:19:11 +0700

    P1 - Langkah 9: buat widget _buildTaskTile

--- lib/views/plan_screen.dart
+++ lib/views/plan_screen.dart
@@ -1,6 +1,7 @@
 import '../models/data_layer.dart';
 import 'package:flutter/material.dart';
 
+// Langkah 6: Membuat StatefulWidget bernama PlanScreen
 class PlanScreen extends StatefulWidget {
   const PlanScreen({super.key});
 
@@ -8,19 +9,21 @@ class PlanScreen extends StatefulWidget {
   State createState() => _PlanScreenState();
 }
 
+// Langkah 7: Inisialisasi objek Plan dan mulai membangun tampilan dasar
 class _PlanScreenState extends State<PlanScreen> {
   Plan plan = const Plan();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ganti ‘Namaku’ dengan nama panggilan Anda
+      // Langkah 8: Menambahkan AppBar, ListView, dan FloatingActionButton
       appBar: AppBar(title: const Text('Master Plan Bhimantara')),
       body: _buildList(),
       floatingActionButton: _buildAddTaskButton(),
     );
   }
 
+  // Langkah 7: Membuat method tombol tambah task baru
   Widget _buildAddTaskButton() {
     return FloatingActionButton(
       child: const Icon(Icons.add),
@@ -36,11 +39,50 @@ class _PlanScreenState extends State<PlanScreen> {
     );
   }
 
+  // Langkah 8: Menampilkan daftar task secara dinamis menggunakan ListView
   Widget _buildList() {
     return ListView.builder(
       itemCount: plan.tasks.length,
       itemBuilder: (context, index) =>
           _buildTaskTile(plan.tasks[index], index),
+    );
+  }
+
+  // Langkah 9: Membuat widget ListTile untuk setiap item task
+  Widget _buildTaskTile(Task task, int index) {
+    return ListTile(
+      // Checkbox untuk menandai status selesai/tidak
+      leading: Checkbox(
+        value: task.complete,
+        onChanged: (selected) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: task.description,
+                  complete: selected ?? false,
+                ),
+            );
+          });
+        },
+      ),
+      // TextFormField untuk menulis/mengubah deskripsi task
+      title: TextFormField(
+        initialValue: task.description,
+        onChanged: (text) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete,
+                ),
+            );
+          });
+        },
+      ),
     );
   }
 }
