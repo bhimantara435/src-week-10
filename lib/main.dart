93ce67bec07b2b79354da9784fa752e8fd34ed6b
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-10-29 14:16:47 +0700

    P1 - Langkah 5: Pindah ke file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import './views/plan_screen.dart';
 
+// Fungsi utama menjalankan aplikasi
 void main() => runApp(MasterPlanApp());
 
+// Widget utama aplikasi
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: PlanScreen(),
+      theme: ThemeData(primarySwatch: Colors.purple), // tema utama
+      home: PlanScreen(), // halaman awal
     );
   }
 }
