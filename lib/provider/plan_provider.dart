a45e77ae663bbaed96048a5f12e1e08fe67c7c67
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-11-03 22:50:55 +0700

    P3 - Langkah 1: Edit PlanProvider

--- lib/provider/plan_provider.dart
+++ lib/provider/plan_provider.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import '../models/data_layer.dart';
 
-class PlanProvider extends InheritedNotifier<ValueNotifier<Plan>> {
+// Langkah 1: Ubah PlanProvider agar mendukung daftar (List) dari beberapa Plan
+class PlanProvider extends InheritedNotifier<ValueNotifier<List<Plan>>> {
   const PlanProvider({
     super.key,
     required Widget child,
-    required ValueNotifier<Plan> notifier,
+    required ValueNotifier<List<Plan>> notifier,
   }) : super(child: child, notifier: notifier);
 
-  static ValueNotifier<Plan> of(BuildContext context) {
+  // Method 'of' digunakan untuk mengambil data dari provider di widget tree
+  static ValueNotifier<List<Plan>> of(BuildContext context) {
     return context
         .dependOnInheritedWidgetOfExactType<PlanProvider>()!
         .notifier!;
