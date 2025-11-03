7d146444e6e5fcde937228e86e4dd45b0b7efb0f
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-11-03 23:26:30 +0700

    P3 - Langkah 10: Pindah ke class _PlanCreatorScreenState

--- lib/views/plan_creator_screen.dart
+++ lib/views/plan_creator_screen.dart
@@ -10,4 +10,15 @@ class PlanCreatorScreen extends StatefulWidget {
   State<PlanCreatorScreen> createState() => _PlanCreatorScreenState();
 }
 
+// Langkah 10 – Tambahkan class _PlanCreatorScreenState
+class _PlanCreatorScreenState extends State<PlanCreatorScreen> {
+  // Langkah 10 – Membuat controller untuk mengatur input teks
+  final textController = TextEditingController();
 
+  @override
+  void dispose() {
+    // Langkah 10 – Membersihkan controller untuk mencegah kebocoran memori
+    textController.dispose();
+    super.dispose();
+  }
+}
