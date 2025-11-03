67395cd7781f7f5350aa92f8e5a8e1793452f2be
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-11-03 23:23:38 +0700

    P3 - Langkah 9: Buat screen baru

---
+++ lib/views/plan_creator_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/data_layer.dart';
+import '../models/plan_provider.dart';
+import 'plan_screen.dart';
+
+class PlanCreatorScreen extends StatefulWidget {
+  const PlanCreatorScreen({super.key});
+
+  @override
+  State<PlanCreatorScreen> createState() => _PlanCreatorScreenState();
+}
+
+
